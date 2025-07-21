b56c5c39241afae4419e0c9c61f167eb6e5f75e5
shrey <pshrey0000@gmail.com>
2025-07-21 23:15:24 +0530

    image changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
-
-type Review = { img: string };
+import { cn } from "@/lib/utils";
+import { Marquee } from "@/components/magicui/marquee";
+type Review = string;
 
 function splitIntoRandomRows(reviews: Review[], rows: number = 6): Review[][] {
   if (reviews.length < rows * 6) {
@@ -29,124 +30,48 @@ function App() {
       }}
     >
       <MarqueeDemoVertical />
+      <p className="sr-only">random text</p>
     </div>
   );
 }
 
-export default App;
-
-import { cn } from "@/lib/utils";
-import { Marquee } from "@/components/magicui/marquee";
-
-const reviews: { img: string }[] = [
-  {
-    img: "/23.webp",
-  },
-  {
-    img: "/9.webp",
-  },
-  {
-    img: "/15.webp",
-  },
-  {
-    img: "/1.png",
-  },
-  {
-    img: "/29.webp",
-  },
-  {
-    img: "/34.webp",
-  },
-  {
-    img: "/7.png",
-  },
-  {
-    img: "/8.webp",
-  },
-  {
-    img: "/2.png",
-  },
-  {
-    img: "/10.webp",
-  },
-  {
-    img: "/11.webp",
-  },
-  {
-    img: "/12.webp",
-  },
-  {
-    img: "/13.webp",
-  },
-  {
-    img: "/14.webp",
-  },
-  {
-    img: "/3.png",
-  },
-  {
-    img: "/16.webp",
-  },
-  {
-    img: "/17.webp",
-  },
-  {
-    img: "/18.webp",
-  },
-  {
-    img: "/19.webp",
-  },
-  {
-    img: "/20.webp",
-  },
-  {
-    img: "/21.webp",
-  },
-  {
-    img: "/22.webp",
-  },
-  {
-    img: "/4.png",
-  },
-  {
-    img: "/24.webp",
-  },
-  {
-    img: "/25.webp",
-  },
-  {
-    img: "/26.webp",
-  },
-  {
-    img: "/27.webp",
-  },
-  {
-    img: "/28.webp",
-  },
-  {
-    img: "/5.png",
-  },
-  {
-    img: "/30.webp",
-  },
-  {
-    img: "/31.webp",
-  },
-  {
-    img: "/32.webp",
-  },
-  {
-    img: "/33.webp",
-  },
-  {
-    img: "/6.png",
-  },
-  {
-    img: "/35.webp",
-  },
-  {
-    img: "/36.webp",
-  },
+const reviews: string[] = [
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/23.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/9.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/15.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/1.png",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/29.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/34.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/7.png",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/8.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/2.png",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/10.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/11.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/12.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/13.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/14.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/3.png",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/16.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/17.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/18.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/19.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/20.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/21.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/22.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/4.png",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/24.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/25.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/26.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/27.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/28.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/5.png",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/30.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/31.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/32.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/33.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/6.png",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/35.webp",
+  "https://neumbsedgujywvok.public.blob.vercel-storage.com/36.webp",
 ];
 
 const rows = splitIntoRandomRows(reviews);
@@ -179,12 +104,12 @@ function MarqueeDemoVertical() {
         className="[--duration:100s] [--gap:8px] p-1 h-full flex-1"
       >
         {firstRow.map((review, index) => (
-          <ReviewCard key={index} {...review} />
+          <ReviewCard key={index} img={review} />
         ))}
       </Marquee>
       <Marquee vertical className="[--duration:60s] [--gap:8px] p-1 flex-1">
         {secondRow.map((review, index) => (
-          <ReviewCard key={index} {...review} />
+          <ReviewCard key={index} img={review} />
         ))}
       </Marquee>
       <Marquee
@@ -192,7 +117,7 @@ function MarqueeDemoVertical() {
         className="hidden sm:flex [--duration:110s] [--gap:8px] p-1 h-full flex-1"
       >
         {thirdRow.map((review, index) => (
-          <ReviewCard key={index} {...review} />
+          <ReviewCard key={index} img={review} />
         ))}
       </Marquee>
       <Marquee
@@ -200,7 +125,7 @@ function MarqueeDemoVertical() {
         className="hidden sm:flex [--duration:95s] [--gap:8px] p-1 flex-1"
       >
         {fourthRow.map((review, index) => (
-          <ReviewCard key={index} {...review} />
+          <ReviewCard key={index} img={review} />
         ))}
       </Marquee>
       <Marquee
@@ -208,7 +133,7 @@ function MarqueeDemoVertical() {
         className="hidden md:flex [--duration:70s] [--gap:8px] p-1 h-full flex-1"
       >
         {fifthRow.map((review, index) => (
-          <ReviewCard key={index} {...review} />
+          <ReviewCard key={index} img={review} />
         ))}
       </Marquee>
       <Marquee
@@ -216,11 +141,11 @@ function MarqueeDemoVertical() {
         className="hidden md:flex [--duration:100s] [--gap:8px] p-1 flex-1"
       >
         {sixthRow.map((review, index) => (
-          <ReviewCard key={index} {...review} />
+          <ReviewCard key={index} img={review} />
         ))}
       </Marquee>
-      {/* <div className="pointer-events-none absolute inset-x-0 top-0 h-1/4 bg-gradient-to-b from-background"></div> */}
-      {/* <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-background"></div> */}
     </div>
   );
 }
+
+export default App;
