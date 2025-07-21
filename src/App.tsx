c49080a5b6d401762cdf3afcd8b037febc64a31c
shrey <pshrey0000@gmail.com>
2025-07-21 23:39:30 +0530

    image changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import "./App.css";
-import { lazy, Suspense } from "react";
-
-// Lazy load the MarqueeDemoVertical component
-const LazyMarqueeDemo = lazy(() => import("./components/MarqueeDemoVertical"));
+import MarqueeDemoVertical from "./components/MarqueeDemoVertical";
 
 function App() {
   return (
@@ -13,10 +10,7 @@ function App() {
           "linear-gradient(160deg, #020617 0%, #081C4F 35%, #0B3B7B 70%, #2A7FD0 100%)",
       }}
     >
-      <Suspense fallback={<div></div>}>
-        <LazyMarqueeDemo />
-      </Suspense>
-      <p className="leading-0 text-[1px]">random text</p>
+      <MarqueeDemoVertical />
     </div>
   );
 }
